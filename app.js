a2c53d4cb4537b6621a8220af8316f05159060a8
Sai Yeshwanth Muddaragadda <84711595+sai2yeshwanth@users.noreply.github.com>
2023-10-06 12:04:51 +0000

    change code React to Jsx

--- app.js
+++ app.js
@@ -1,26 +1,11 @@
 import React from 'react';
 import  ReactDOM  from 'react-dom/client'
-
-const parent = React.createElement("div", {
-    id: "parent"
-},
-   [
-    React.createElement("div", {id :"child"}, [
-        React.createElement("h1",{},"Iam an h1 tag"),
-        React.createElement("h2",{},"Iam an h2 tag")
-    ]),
-    React.createElement("div", {id :"child2"}, [
-        React.createElement("h1",{},"Iam an h1 tag"),
-        React.createElement("h2",{},"Iam an h2 tag")
-    ])
-   ]
+//jsx => Babel transpiles it to React.createElement => ReactElement-js Object => HTMLElement(render)
+const jsxHeading = (
+    <h1 className='head' >
+        Namaste React Using JSX
+    </h1>
 )
-
-
-const heading = React.createElement("h1", {
-    id: "heading",
-
-}, "Hello world from React!");
-console.log(heading);//object
+console.log(jsxHeading);//object
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(jsxHeading);
